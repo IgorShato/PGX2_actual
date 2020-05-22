55643e6d3e17b60da904671a24f7dcb490f8239e
Igor Shatokhin <30746701+IgorShato@users.noreply.github.com>
2020-05-22 10:18:44 +0300

    Update report.php

--- report.php
+++ report.php
@@ -27,19 +27,10 @@ try{
 		$secondStepColumns = $_POST['drugs'];
 	}
 
-	// данные пациента
-	$InputId = $values['InputId'];
-	$InputSurname = $values['InputSurname'];
-	$InputFirstName = $values['InputFirstName'];
-	$InputSecondName = $values['InputSecondName'];
-	$InputBirth = $values['InputBirth'];
-	$InputHeight = $values['InputHeight'];
-	$InputWeight = $values['InputWeight'];
-	$InputSex = $values['InputSex'];
-	$InputRase = $values['InputRase'];
-	$InputDate = $values['InputDate'];
-	$InputNameDoctor = $values['InputNameDoctor'];
-
+	if (isset($_POST['kPolSum'])){
+		$kPolSumColumns = $_POST['kPolSum'];
+		$dada = 'dasd';
+	}
 
 	//подключаем библиотеку
 	include('library/tcpdf.php'); 
@@ -58,17 +49,16 @@ try{
 	//обрыв страницы
 	$pdf->SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
 
-	/**
-	 * страница 1
-	 */
-
-	$pdf->AddPage();
+	//страница 1 
+	$pdf->AddPage('P', 'A4');
 			
 	//вертикальная позиция для колонки
 	$y = $pdf->getY();
 	
 	//цвет для левой колонки
 	$pdf->SetFillColor(75, 120, 114);
+
+	$left_column = '';
 
 	//левая колонка
 	$pdf->writeHTMLCell(40, '295', '1', '1', $left_column, 0, 0, 1, true, 'J', true);
@@ -169,11 +159,10 @@ try{
 	$pdf->WriteHTMLCell(140, 20, '60', '259', "$sign", 0,0);
 
 
-	/**
-	 * страница 2
-	 */
-		$pdf->AddPage();
+	//страница 2
+		$pdf->AddPage('P', 'A4');
 
+		
 	//вертикальная позиция для столбца
 	$y = $pdf->getY();
 
@@ -186,8 +175,7 @@ try{
 
 	//вертикальный текст - начало
 	$pdf->StartTransform();
-
-	//поворот текста
+	// Rotate of degrees
 	$pdf->Rotate(90, 58, 145);
 	$pdf->SetDrawColor(255);
 	$pdf->SetTextColor(255);
@@ -244,7 +232,6 @@ try{
 	//вывод таблицы
 	$pdf->WriteHTMLCell(140, 30, '23', '20', "$table1", 1,0);
 
-    //таблица активности генов
 	$tableGenesRow = '';
 	if (isset($_POST['columns'])){
 
@@ -257,6 +244,7 @@ try{
 				<td>'.$column[3].'</td>
 				<td>'.$column[4].'</td>
 				<td>'.$column[5].'</td>
+			  <td>N</td>
 			</tr>';
 		}
 	}
@@ -293,72 +281,8 @@ try{
 
 	//вывод таблицы
 	$pdf->WriteHTMLCell(165, '', '25', '64', "$table_genes", 0,0);
-
-/*
-		// треугольники
-		$style = array('width' => 0.5, 'cap' => 'butt', 'join' => 'miter', 'dash' => 0, 'phase' => 0, 'color' => array(0, 0, 0));
-		$style2 = array('width' => 0.5, 'cap' => 'butt', 'join' => 'miter', 'dash' => 0, 'color' => array(0, 0, 0));
-		$style3 = array('width' => 0.5, 'cap' => 'round', 'join' => 'round', 'dash' => 0, 'color' => array(0, 0, 0));
-		$style4 = array('L' => 0,
-										'T' => array('width' => 0.25, 'cap' => 'butt', 'join' => 'miter', 'dash' => '20,10', 'phase' => 10, 'color' => array(100, 100, 255)),
-										'R' => array('width' => 0.50, 'cap' => 'round', 'join' => 'miter', 'dash' => 0, 'color' => array(50, 50, 127)),
-										'B' => array('width' => 0.75, 'cap' => 'square', 'join' => 'miter', 'dash' => '30,10,5,10'));
-		$style5 = array('width' => 0.5, 'cap' => 'butt', 'join' => 'miter', 'dash' => 0, 'color' => array(49, 21, 168));
-		$style6 = array('width' => 0.5, 'cap' => 'butt', 'join' => 'miter', 'dash' => '10,10', 'color' => array(0, 128, 0));
-		$style7 = array('width' => 0.5, 'cap' => 'butt', 'join' => 'miter', 'dash' => 0, 'color' => array(168, 24, 21));
 	
-	
-		$pdf->Line(40, 219, 40, 270, $style2); //вертикальная линия
-		$pdf->Line(42, 244, 196, 244, $style3); //горизонтальная линия
-	
-		//текст
-		$pdf->WriteHTMLCell(15, 10, '27', '218', "<small>200%</small>");
-		$pdf->WriteHTMLCell(15, 10, '27', '230', "<small>150%</small>");
-		$pdf->WriteHTMLCell(15, 10, '27', '242', "<small>100%</small>");
-		$pdf->WriteHTMLCell(15, 10, '29', '254', "<small>50%</small>");
-		$pdf->WriteHTMLCell(15, 10, '30', '266', "<small>0%</small>");
-	
-		//текст
-		$pdf->WriteHTMLCell(30, 20, '28', '277', "<small>Активность</small>");
-		$pdf->WriteHTMLCell(30, 20, '50', '277', "<small>CYP2C19*2</small>");
-		$pdf->WriteHTMLCell(30, 20, '72', '277', "<small>CYP2C19*3</small>");
-		$pdf->WriteHTMLCell(30, 20, '94', '277', "<small>CYP2C19*17</small>");
-		$pdf->WriteHTMLCell(30, 20, '118', '277', "<small>CYP2D6*4</small>");
-		$pdf->WriteHTMLCell(30, 20, '138', '277', "<small>CYP3A4*22</small>");
-		$pdf->WriteHTMLCell(30, 20, '160', '277', "<small>CYP3A5*3</small>");
-		$pdf->WriteHTMLCell(30, 20, '182', '277', "<small>ABCB1*6</small>");
-	
-		// треугольник вверх 
-		$pdf->RegularPolygon(104, 238, 12, 3, 60, '', 'DF', $style6, array(50, 59, 159));
-		// текст треугольника
-		$persent_up = '
-		<p>+95%</p>
-		<style>
-		p {
-			color: #FFFFFF;
-			font-size: 9px;
-		}
-		</style> 
-		';
-		$pdf->WriteHTMLCell(20, 20, '98', '235', $persent_up);
-	
-		// треугольник низ 
-		$pdf->RegularPolygon(125, 250, 12, 3, 0, '', 'DF', $style6, array(168, 24, 48));
-		// текст треугольника 
-		$persent_down = '
-		<p>-95%</p>
-		<style>
-		p {
-			color: #FFFFFF;
-			font-size: 9px;
-		}
-		</style>
-		';
-		$pdf->WriteHTMLCell(20, 20, '120', '249', $persent_down);
-	
-	*/
-	
-	// текст  
+	//текст 
 	$text_page2 = '
 	<p>Имеются данные, свидетельствующие о наличии генетически обусловленных отклонений в скорости метаболизма, что может увеличить риск развития нежелательных реакций на назначаемые лекарственные препараты и, в связи с этим, недостаточной эффективности терапии.</p>
 	<p>Снижение активности CYP2C19, CYP2D6, CYP3A5 и ABCB1 будет приводить к замедлению элиминации большей части лекарственных средств: антидепрессантов, антипсихотиков, транквилизаторов и антиконвульсантов, то есть к возрастанию риска развития нежелательных реакций. 
@@ -372,42 +296,84 @@ try{
 	';
 
 	// вывод текста
-	$pdf->WriteHTMLCell(160, 90, '30', '225', "$text_page2", 0,0);
+	$pdf->WriteHTMLCell(160, 90, '30', '150', "$text_page2", 0,0);
 
-	
-         /**
-	 * страница 3
-	 */
-	   
-	$pdf->AddPage('L', 'A4');
+	// треугольники
+	$style = array('width' => 0.5, 'cap' => 'butt', 'join' => 'miter', 'dash' => 0, 'phase' => 0, 'color' => array(0, 0, 0));
+	$style2 = array('width' => 0.5, 'cap' => 'butt', 'join' => 'miter', 'dash' => 0, 'color' => array(0, 0, 0));
+	$style3 = array('width' => 0.5, 'cap' => 'round', 'join' => 'round', 'dash' => 0, 'color' => array(0, 0, 0));
+	$style4 = array('L' => 0,
+									'T' => array('width' => 0.25, 'cap' => 'butt', 'join' => 'miter', 'dash' => '20,10', 'phase' => 10, 'color' => array(100, 100, 255)),
+									'R' => array('width' => 0.50, 'cap' => 'round', 'join' => 'miter', 'dash' => 0, 'color' => array(50, 50, 127)),
+									'B' => array('width' => 0.75, 'cap' => 'square', 'join' => 'miter', 'dash' => '30,10,5,10'));
+	$style5 = array('width' => 0.5, 'cap' => 'butt', 'join' => 'miter', 'dash' => 0, 'color' => array(49, 21, 168));
+	$style6 = array('width' => 0.5, 'cap' => 'butt', 'join' => 'miter', 'dash' => '10,10', 'color' => array(0, 128, 0));
+	$style7 = array('width' => 0.5, 'cap' => 'butt', 'join' => 'miter', 'dash' => 0, 'color' => array(168, 24, 21));
+
+
+	$pdf->Line(40, 219, 40, 270, $style2); //вертикальная линия
+	$pdf->Line(42, 244, 196, 244, $style3); //горизонтальная линия
+
+	//текст
+	$pdf->WriteHTMLCell(15, 10, '27', '218', "<small>200%</small>");
+	$pdf->WriteHTMLCell(15, 10, '27', '230', "<small>150%</small>");
+	$pdf->WriteHTMLCell(15, 10, '27', '242', "<small>100%</small>");
+	$pdf->WriteHTMLCell(15, 10, '29', '254', "<small>50%</small>");
+	$pdf->WriteHTMLCell(15, 10, '30', '266', "<small>0%</small>");
+
+	//текст
+	$pdf->WriteHTMLCell(30, 20, '28', '277', "<small>Активность</small>");
+	$pdf->WriteHTMLCell(30, 20, '50', '277', "<small>CYP2C19*2</small>");
+	$pdf->WriteHTMLCell(30, 20, '72', '277', "<small>CYP2C19*3</small>");
+	$pdf->WriteHTMLCell(30, 20, '94', '277', "<small>CYP2C19*17</small>");
+	$pdf->WriteHTMLCell(30, 20, '118', '277', "<small>CYP2D6*4</small>");
+	$pdf->WriteHTMLCell(30, 20, '138', '277', "<small>CYP3A4*22</small>");
+	$pdf->WriteHTMLCell(30, 20, '160', '277', "<small>CYP3A5*3</small>");
+	$pdf->WriteHTMLCell(30, 20, '182', '277', "<small>ABCB1*6</small>");
+
+	// треугольник вверх 
+	$pdf->RegularPolygon(104, 238, 12, 3, 60, '', 'DF', $style6, array(50, 59, 159));
+	// текст треугольника
+	$persent_up = '
+	<p>+95%</p>
+	<style>
+	p {
+		color: #FFFFFF;
+		font-size: 9px;
+	}
+	</style> 
+	';
+	$pdf->WriteHTMLCell(20, 20, '98', '235', $persent_up);
+
+	// треугольник низ 
+	$pdf->RegularPolygon(125, 250, 12, 3, 0, '', 'DF', $style6, array(168, 24, 48));
+	// текст треугольника 
+	$persent_down = '
+	<p>-95%</p>
+	<style>
+	p {
+		color: #FFFFFF;
+		font-size: 9px;
+	}
+	</style>
+	';
+	$pdf->WriteHTMLCell(20, 20, '120', '249', $persent_down);
+
+
+
+	//страница 3
+		$pdf->AddPage('L', 'A4');
 		
-	//цвет фона
+		//таблица
+	// цвет фона
 	$pdf->SetFillColor(75, 120, 114);
 
 	// цвет текста
 	//$pdf->SetTextColor(255, 255, 255);
 
-	$tableDrugsRow = '';
-	if (isset($_POST['drugs'])){
-		
-
-		foreach($secondStepColumns as $key => $column){
-			$tableDrugsRow .= 
-			'<tr>
-				<td style="color: #4B7872">'.$column[0].'</td>
-				<td></td>
-				<td></td>
-				<td></td>
-				<td></td>
-				<td></td>
-				<td></td>
-			</tr>';
-		}
-	}
-
 	// хедер таблицы
-	$header_table_extended = '
-	<br><h4>Название группы</h4>
+	$header_table_spec = '
+	<br><h4>СПЕЦИФИЧЕСКИЕ РЕКОМЕНДАЦИИ</h4>
 	<style>
 	h4 {
 		font-size: 14px;
@@ -418,22 +384,65 @@ try{
 	</style>
 	';
 
-	$pdf->writeHTMLCell(288, '24', '4', '4', $header_table_extended, 0, 0, 1, true, 'J', true);
-
+	$pdf->writeHTMLCell(288, '24', '4', '4', $header_table_spec, 0, 0, 1, true, 'J', true);
 
 	//таблица
-	$table_extended = '
+	$table_spec = '
 	<table>
-		<tr>
-		<th style="color: #FFFFFF; background-color: #5DACA1;">СИОЗС</th>
-		<th style="color: #FFFFFF; background-color: #5DACA1;">РЕКОМЕНДАЦИИ</th>
-		<th style="color: #FFFFFF; background-color: #5DACA1;">ФАРМАКО<br>ДИНАМИКА</th>
-		<th style="color: #FFFFFF; background-color: #5DACA1;">ДЕЙСТВИЕ</th>
-		<th style="color: #FFFFFF; background-color: #5DACA1;">ФАРМАКО<br>КИНЕТИКА</th>
-		<th style="color: #FFFFFF; background-color: #5DACA1;">ДЕЙСТВИЕ</th>
-		<th style="color: #FFFFFF; background-color: #5DACA1;">ПОЛИПРАГМАЗИЯ<br>И ДРУГИЕ ФАКТОРЫ</th>
-		</tr>
-		'.$tableDrugsRow.'
+	<tr>
+	<th style="color: #FFFFFF; background-color: #5DACA1;">СИОЗС</th>
+	<th style="color: #FFFFFF; background-color: #5DACA1;">РЕКОМЕНДАЦИИ</th>
+	<th style="color: #FFFFFF; background-color: #5DACA1;">ФАРМАКО<br>ДИНАМИКА</th>
+	<th style="color: #FFFFFF; background-color: #5DACA1;">ДЕЙСТВИЕ</th>
+	<th style="color: #FFFFFF; background-color: #5DACA1;">ФАРМАКО<br>КИНЕТИКА</th>
+	<th style="color: #FFFFFF; background-color: #5DACA1;">ДЕЙСТВИЕ</th>
+	<th style="color: #FFFFFF; background-color: #5DACA1;">ПОЛИПРАГМАЗИЯ<br>И ДРУГИЕ ФАКТОРЫ</th>
+	</tr>
+	<tr>
+	<td style="color: #4B7872">Серталин<br>Золофт&reg;</td>
+	<td></td>
+	<td></td>
+	<td></td>
+	<td></td>
+	<td></td>
+	<td></td>
+	</tr>
+	<tr>
+	<td style="color: #4B7872">Флувоксамин<br>Феварин&reg;</td>
+	<td></td>
+	<td></td>
+	<td></td>
+	<td></td>
+	<td></td>
+	<td></td>
+	</tr>
+	<tr>
+	<td style="color: #4B7872">Флуоксетин<br>Прозак&reg;</td>
+	<td></td>
+	<td></td>
+	<td></td>
+	<td></td>
+	<td></td>
+	<td></td>
+	</tr>
+	<tr>
+	<td style="color: #4B7872">Циталопрам<br>Ципрамил&reg;</td>
+	<td></td>
+	<td></td>
+	<td></td>
+	<td></td>
+	<td></td>
+	<td></td>
+	</tr>
+	<tr>
+	<td style="color: #4B7872">Эскиталопрам<br>Ципралек&reg;</td>
+	<td></td>
+	<td></td>
+	<td></td>
+	<td></td>
+	<td></td>
+	<td></td>
+	</tr>
 	</table>
 
 	<style>
@@ -456,15 +465,12 @@ try{
 	';
 
 	//вывод таблицы
-	$pdf->WriteHTMLCell(290, 15, '3', '30', "$table_extended", 0, 0);
+	$pdf->WriteHTMLCell(290, 15, '3', '30', "$table_spec", 0, 0);
 
-	 
-         /**
-	 * страница 4
-	 */
-	   
-	$pdf->AddPage('L', 'A4');
+	//страница 4
+		$pdf->AddPage('L', 'A4');
 		
+		//таблица
 	//цвет фона
 	$pdf->SetFillColor(75, 120, 114);
 
@@ -491,7 +497,7 @@ try{
 
 	// хедер таблицы
 	$header_table_extended = '
-	<br><h4>Название группы</h4>
+	<br><h4>РАСШИРЕННЫЕ РЕКОМЕНДАЦИИ</h4>
 	<style>
 	h4 {
 		font-size: 14px;
@@ -541,6 +547,89 @@ try{
 
 	//вывод таблицы
 	$pdf->WriteHTMLCell(290, 15, '3', '30', "$table_extended", 0, 0); 
+
+
+
+		//страница 5
+		$pdf->AddPage('L', 'A4');
+		
+		//таблица
+	//цвет фона
+	$pdf->SetFillColor(75, 120, 114);
+
+	// цвет текста
+	//$pdf->SetTextColor(255, 255, 255);
+
+	$tableKPolSumRow = '';
+	if (isset($_POST['kPolSum'])){
+		
+
+		foreach($kPolSumColumns as $key => $column){
+			$tableKPolSumRow .= 
+			'<tr>
+				<td>'.$column['gen'].'</td>
+				<td>'.$column['kPolSum'].'</td>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+			</tr>';
+		}
+	}
+
+	// хедер таблицы
+	$header_table_newName = '
+	<br><h4>РАСШИРЕННЫЕ РЕКОМЕНДАЦИИ</h4>
+	<style>
+	h4 {
+		font-size: 14px;
+		text-align: center;
+		color: #FFFFFF;
+		font-weight: regular;
+	}
+	</style>
+	';
+
+	$pdf->writeHTMLCell(288, '24', '4', '4', $header_table_newName, 0, 0, 1, true, 'J', true);
+
+
+	//таблица
+	$table_newName = '
+	<table>
+		<tr>
+		<th style="color: #FFFFFF; background-color: #5DACA1;">СИОЗС</th>
+		<th style="color: #FFFFFF; background-color: #5DACA1;">РЕКОМЕНДАЦИИ</th>
+		<th style="color: #FFFFFF; background-color: #5DACA1;">ФАРМАКО<br>ДИНАМИКА</th>
+		<th style="color: #FFFFFF; background-color: #5DACA1;">ДЕЙСТВИЕ</th>
+		<th style="color: #FFFFFF; background-color: #5DACA1;">ФАРМАКО<br>КИНЕТИКА</th>
+		<th style="color: #FFFFFF; background-color: #5DACA1;">ДЕЙСТВИЕ</th>
+		<th style="color: #FFFFFF; background-color: #5DACA1;">ПОЛИПРАГМАЗИЯ<br>И ДРУГИЕ ФАКТОРЫ</th>
+		</tr>
+		'.$tableKPolSumRow.'
+	</table>
+
+	<style>
+	th {
+		font-size: 11.5px;
+		color: #FFFFFF;
+		background-color: #4B7872;
+		text-align: center;
+		border: solid 1px #FFFFFF;
+	}
+	td {
+		font-size: 10px;
+		text-align: center;
+		font-weight: bold;
+		color: #000000;
+		border: solid 1px #A5C5C1;
+		height: 40px;
+	}
+	</style>
+	';
+
+	//вывод таблицы
+	$pdf->WriteHTMLCell(290, 15, '3', '30', "$table_newName", 0, 0); 
 	$pdf->Output('PharmacoGenomeX2 - Report.pdf', 'I');
 
 	//вывод данных
@@ -549,4 +638,5 @@ try{
 }catch (PDOExeption $e){
 	return $e->getMessage;
 }
+ 
  
